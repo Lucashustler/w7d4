d8f32eaffeb45246bd73ff860c165d02dd1c59fc
Lucas Grandviergne <lucasgdve@gmail.com>
2019-11-14 22:20:08 +0100

    lucas commit

--- script_3.js
+++ script_3.js
@@ -1,30 +1,16 @@
-// // on demande à l'utilisateur d'entrer le nombre d'étage qu'il souhaite voir afficher en pyramide
-
-// let Nombre = prompt("Combien d'étage voulez-vous à votre pyramide ?")
-
-// let Espace = Nombre
-// let Etage = 1
-
-// for (let compteur = 1; compteur <= Nombre; compteur += 1){
-//     Etage += compteur
-//     Espace -= Etage
-// }
-
-// console.log(`${Espace}*)
-
-
 // EXERICE 3
 
 // On demande à l'utilsateur de rentrer le nombre d'étages qu'il veut
 let userInput = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?");
 
-// On définit le nombre d'espaces avant la dièse en enlevant 1 au userInput
-// (pour laisser place à la dièse)
+// On définit la première ligne soit le nombre d'espaces avant le dièse en enlevant 1 au userInput
 let nb_spaces = userInput - 1
 
-for (number = 1; number <= userInput; number++) {
+// on définit:
+// - 
+for (counter = 1; counter <= userInput; counter++) {
 	// En JS, repeat permet de répéter le string le nombre de fois voulu
-	console.log(" ".repeat(nb_spaces) + "#".repeat(number));
+	console.log(" ".repeat(nb_spaces) + "#".repeat(counter));
 	// On réduit le nombre d'espaces de 1 à chaque passage de boucle.
 	nb_spaces--
 }
